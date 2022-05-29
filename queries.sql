6c866f9c362fa5b1b3e6d59f3d529b6f66869e81
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 20:33:11 +0300

    Update Find all animals with a weight between 10.4kg and 17.3kg

--- queries.sql
+++ queries.sql
@@ -20,3 +20,6 @@ SELECT * FROM animals WHERE neutered = TRUE;
 
 -- Select all animals whose name is not Gabumon
 SELECT * FROM animals WHERE name != 'Gabumon';
+
+-- Find all animals with a weight between 10.4kg and 17.3kg
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
