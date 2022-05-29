0dbe50af461e09e1d81c97b9946d510b6355d115
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 13:54:25 +0300

    Update to data.sql comment for documentation

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-/* Populate database with sample data. */
+/* Populate vet_clinic database with data. */
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2, 'Gabumon', '2018-11-15', 2, TRUE, 8);
