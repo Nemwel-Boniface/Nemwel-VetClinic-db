9958d3f4908b11dd853ef96bb42c8b304082c3e5
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 13:53:04 +0300

    Update inserted the forth animal details

--- data.sql
+++ data.sql
@@ -3,3 +3,4 @@
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2, 'Gabumon', '2018-11-15', 2, TRUE, 8);
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(3, 'Pikachu', '2021-01-7', 1, FALSE, 15.04);
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(4. 'Devimon', '2017-5-12', 5, TRUE, 11);
