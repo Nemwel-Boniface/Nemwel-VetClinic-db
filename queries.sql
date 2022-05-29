bd9cee7e6310e603a520b9e26b11652969f4f38a
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 19:41:32 +0300

    Update Find all animals whose name ends in 'mon'

--- queries.sql
+++ queries.sql
@@ -1,5 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- Select all animals whose name ends in "mon".
+SELECT * FROM animals WHERE name LIKE '%mon';
+
 -- Select all from animals table which are neutered
 SELECT * FROM animals WHERE neutered = TRUE;
 
