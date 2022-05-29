cdebff962c78233046959ae5426d27c50610a0f1
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 20:12:54 +0300

    Update List the name of all animals that are neutered and have less than 3 escape attempts

--- queries.sql
+++ queries.sql
@@ -3,6 +3,9 @@
 -- Select all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
 
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+
 -- Select all from animals table which are neutered
 SELECT * FROM animals WHERE neutered = TRUE;
 
