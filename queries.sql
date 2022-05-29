4d1363c6a4189be248e1953d11d5e558a40589df
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 20:18:31 +0300

    update List the name of all animals born between 2016 and 2019.

--- queries.sql
+++ queries.sql
@@ -3,6 +3,9 @@
 -- Select all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
 
+-- List the name of all animals born between 2016 and 2019.
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
 
