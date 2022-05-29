e878284def8971d438f639bdb072c1017040eb52
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 14:17:48 +0300

    Update select all animals who have been neutered

--- queries.sql
+++ queries.sql
@@ -1,4 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- Select all from animals table which are neutered
+SELECT * FROM animals WHERE neutered = TRUE;
+
 -- Select all animals whose name is not Gabumon
 SELECT * FROM animals WHERE name != 'Gabumon';
