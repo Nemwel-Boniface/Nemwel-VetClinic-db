ce4276a5d6bfb0e6e5e78eade9dc9554c4290063
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 13:50:42 +0300

    Update inserted the first animal details

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
