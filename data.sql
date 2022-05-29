252232b26b55bfad61dab85cdfa4da170284e201
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-05-29 13:51:34 +0300

    Update inserted the second animal details

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1, 'Agumon', '2020-02-03', 0, TRUE, 10.23);
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2, 'Gabumon', '2018-11-15', 2, TRUE, 8);
