66674efc9913e85111added7eb957303d5385fac
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-06-01 14:01:13 +0300

    Update inserted data for fifth owner

--- data.sql
+++ data.sql
@@ -17,3 +17,4 @@ INSERT INTO owners(full_name, age) VALUES('Sam Smith', 34);
 INSERT INTO owners(full_name, age) VALUES('Jennifer Orwell', 19);
 INSERT INTO owners(full_name, age) VALUES('Bob', 45);
 INSERT INTO owners(full_name, age) VALUES('Melody Pond', 77);
+INSERT INTO owners(full_name, age) VALUES('Dean Winchester', 14);
